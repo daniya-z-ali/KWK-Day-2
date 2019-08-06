061bab7237529eab3ffe3c6bf12b491d6e5b9400
Daniya Z Ali <daniyazali@Apples-MacBook-Pro-16.local>
2019-08-06 10:34:37 -0400

    Intial commit

---
+++ Day 2 Warm-Up.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Kode with Klossy Camp - Day 2 Warmup
+
+
+//1. Declare a variable named favUniversity
+//var favUniversity = "Cambridge"
+
+//2. Print a statement about your variable. Be sure to use interpolation.
+//if favUniveristy == "Cambridge" {print("\(favUniveristy) is in England!")}
+
+//3. Change your favUniversity.
+var favUniveristy = "Oxford"
+
+//4. Print a statement about your new favUniversity.
+if favUniveristy == "Oxford" {print("\(favUniveristy) is also in England!")}
+
+//5. Declare a constant with the city your new favorite university is located in named favCity.
+let favCity = "Oxford the city"
+
+//6. Print a statement about your constant.
+if favCity == "Oxford the city" {print("It looks fun!")}
+
+//7. Extension: Concatenate two strings. It can be about anything.
+var string1 = "We are in New York City!"
+var string2 = "It is a beautiful day!"
+var final = "\(string1) \(string2)"
+print(final)
+var final2 = string1 + " " + string2
+print(final2)
+
+//8. Print the strings using interpolation.
+print("The view is beautiful! \(final)")
